3a0279e155d812e5c44ff42ce4ef577e530e5c4d
afterburn25 <afterburn32@gmail.com>
2026-09-07 03:00:27 -0500

    Add real-time scout exploration simulation

--- src/Game/GameVersion.cs
+++ src/Game/GameVersion.cs
@@ -2,5 +2,5 @@ namespace Game;
 
 public static class GameVersion
 {
-    public const string Current = "0.0.2-dev.1";
+    public const string Current = "0.0.3-dev.1";
 }
---
+++ src/Game/Simulation/Models/FleetState.cs
@@ -0,0 +1,24 @@
+using System.Numerics;
+
+namespace Game.Simulation.Models;
+
+public sealed class FleetState
+{
+    public required int Id { get; init; }
+    public required int CivilizationId { get; init; }
+    public required string Name { get; init; }
+    public required FleetRole Role { get; init; }
+    public required Vector2 Position { get; set; }
+    public int? CurrentSystemId { get; set; }
+    public int? DestinationSystemId { get; set; }
+    public double StrategicSpeed { get; init; } = 22.0;
+    public float SensorRange { get; init; } = 135.0f;
+}
+
+public enum FleetRole
+{
+    Scout,
+    Science,
+    Colony,
+    Military,
+}
