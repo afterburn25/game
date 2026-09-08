385bf3cca11fbf48425852ecbddafe24a6384f95
afterburn25 <afterburn32@gmail.com>
2026-09-08 11:59:29 -0500

    Expose core UI icons through runtime library

--- src/Game/Presentation/VisualIconLibrary.cs
+++ src/Game/Presentation/VisualIconLibrary.cs
@@ -13,6 +13,15 @@ public static class VisualIconLibrary
 {
     private static readonly Dictionary<string, Texture2D> Cache = new(StringComparer.Ordinal);
 
+    public const string PausePath = "res://assets/visual/icons/core/icon_hud_pause.svg";
+    public const string SpeedPath = "res://assets/visual/icons/core/icon_hud_speed.svg";
+    public const string SavePath = "res://assets/visual/icons/core/icon_hud_save.svg";
+    public const string SupportPath = "res://assets/visual/icons/core/icon_hud_support.svg";
+    public const string ResearchPath = "res://assets/visual/icons/core/icon_action_research.svg";
+    public const string ConstructionPath = "res://assets/visual/icons/core/icon_action_construction.svg";
+    public const string ExplorationPath = "res://assets/visual/icons/core/icon_system_exploration.svg";
+    public const string LogisticsPath = "res://assets/visual/icons/core/icon_system_logistics.svg";
+    public const string RelationsPath = "res://assets/visual/icons/core/icon_system_relations.svg";
     public const string SurveyDetectedPath = "res://assets/visual/icons/map/icon_map_detected.svg";
     public const string SurveyPartialPath = "res://assets/visual/icons/map/icon_map_partially_surveyed.svg";
     public const string SurveyFullPath = "res://assets/visual/icons/map/icon_map_fully_surveyed.svg";
@@ -23,6 +32,15 @@ public static class VisualIconLibrary
     public const string PatrolCorvettePath = "res://assets/visual/icons/ships/icon_ship_patrol_corvette.svg";
     public const string ColonyShipPath = "res://assets/visual/icons/ships/icon_ship_colony_ship.svg";
 
+    public static Texture2D Pause => Get(PausePath);
+    public static Texture2D Speed => Get(SpeedPath);
+    public static Texture2D Save => Get(SavePath);
+    public static Texture2D Support => Get(SupportPath);
+    public static Texture2D Research => Get(ResearchPath);
+    public static Texture2D Construction => Get(ConstructionPath);
+    public static Texture2D Exploration => Get(ExplorationPath);
+    public static Texture2D Logistics => Get(LogisticsPath);
+    public static Texture2D Relations => Get(RelationsPath);
     public static Texture2D SurveyDetected => Get(SurveyDetectedPath);
     public static Texture2D SurveyPartial => Get(SurveyPartialPath);
     public static Texture2D SurveyFull => Get(SurveyFullPath);
